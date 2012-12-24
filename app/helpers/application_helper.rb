1932cfb626e0ea095e6d7e7840dfdbe1fd422da6
Sudo Myju <sudomyju@gmail.com>
2012-12-24 02:04:52 +0100

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Returns title for any page,
+  # page could provide optional string for function to append 
+  def full_title(page_title)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
