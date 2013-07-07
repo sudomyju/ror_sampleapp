329693129e29e6887903a00da97b4e6c93cc7b48
Sudo Myju <sudomyju@gmail.com>
2013-07-07 11:21:24 +0200

    Finish sign in

---
+++ db/structure.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "users" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar(255), "email" varchar(255), "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL, "password_digest" varchar(255));
+CREATE TABLE "schema_migrations" ("version" varchar(255) NOT NULL);
+CREATE UNIQUE INDEX "unique_schema_migrations" ON "schema_migrations" ("version");
+INSERT INTO schema_migrations (version) VALUES ('20121225092155');
+
+INSERT INTO schema_migrations (version) VALUES ('20121225175059');
+
+INSERT INTO schema_migrations (version) VALUES ('20130423120328');
+
+INSERT INTO schema_migrations (version) VALUES ('20130704221319');
