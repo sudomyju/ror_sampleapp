72fa19596a132927c1128dfdd2f830bccc00f2f5
Sudo Myju <sudomyju@gmail.com>
2013-08-18 11:48:08 +0200

    Added main users page aka users list

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -29,15 +29,17 @@ describe "Authentication" do
     #    click_button "Sign in"
     #  end
     # ...should be solved by .. 
-      before { valid_sigin(user) }
+    #  before { valid_sigin(user) }
 
       #even another before should be here .... 
-      #before { sign_in user }
+      before { sign_in user }
+
       it { should have_title(user.name) }
-      it { should have_link('Profile', href: user_path(user)) }
-      it { should have_link('Settings', href: edit_user_path(user)) }
-      it { should have_link('Sign out', href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
+      it { should have_link('Users',        href: users_path) }
+      it { should have_link('Profile',      href: user_path(user)) }
+      it { should have_link('Settings',     href: edit_user_path(user)) }
+      it { should have_link('Sign out',     href: signout_path) }
+      it { should_not have_link('Sign in',  href: signin_path) }
 
       describe "followed by signout" do
         before { click_link "Sign out" }
@@ -78,6 +80,11 @@ describe "Authentication" do
         describe "submitting to the update action" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_ath }
+          it { should have_title('Sign in') }
         end
       end
     end
